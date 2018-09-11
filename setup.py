e0f39e639f59b09300f7524e4c8169d424d47877
Chuck Mo <chuck@moosejudge.com>
2018-09-11 15:34:45 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+'''py_motools setup.py'''
+
+from setuptools import find_packages, setup
+
+setup(
+    name='motools',
+    version='0.1.0',
+
+    packages=find_packages(exclude=['tests']),
+
+    author='Chuck Mo',
+    author_email='chuck@moosejudge.com',
+
+    description='Moosejudge Tools',
+    long_description='Chuck experimenting with linear algebra',
+    keywords='linear algebra math',
+    license='GPLv3',
+
+
+    #install_requires=[],
+
+    url='https://gitlab.com/TheChuckMo/py_motool',
+    project_urls={
+        "Source Code": "https://gitlab.com/TheChuckMo/py_motool",
+        }
+    # Include default config
+    #include_package_data=True,
+    #package_data={
+    #    'config': ['config.yml'],
+    #},
+
+    # Setup console script
+    # entry_points={
+    #     'console_scripts': [
+    #         'mocli = motools.app:cli',
+    #     ],
+    # },
+
+    # sometimes we need more...
+)
